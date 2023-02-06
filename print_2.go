b5319cade642c02d81f2f1719a9a998edef18fb1
kim130727 <47652911+kim130727@users.noreply.github.com>
2023-02-07 00:53:30 +0900

    Create print_2.go

---
+++ print_2.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+    var num1 int = 1
+    var num2 int = 2
+    fmt.Print("Hello goorm!", num1, num2, "\n")
+    fmt.Println("Hello goorm!", num1, num2)
+    fmt.Printf("num1의 값은:%d num2의 값은:%d\n", num1, num2)
+}
