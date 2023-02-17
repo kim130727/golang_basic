5add1d6ba033dc8ff2376c7a060fa3730da3fa52
kim130727 <47652911+kim130727@users.noreply.github.com>
2023-02-17 22:00:26 +0900

    Create 0009.go

---
+++ 0009.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+const username = "kim"
+func main() {
+	const a int = 1    
+    const b, d= 10, 20 //여러 값을 초기화할 수 있음
+	const c = "goorm"
+	
+	fmt.Println(username)
+}
